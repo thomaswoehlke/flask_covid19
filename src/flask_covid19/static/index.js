e11ac3c9be7cf41b311a2f38864fb46b8a3d48fd
thomaswoehlke <thomas.woehlke@gmail.com>
2021-06-11 14:43:14 +0200

    work

--- src/flask_covid19/static/index.js
+++ src/flask_covid19/static/index.js
@@ -1,11 +1,11 @@
-import "/static/vendor/bootswatch/superhero/bootstrap.min.css";
+import "/vendor/bootswatch/superhero/bootstrap.min.css";
 // Note: Replace ^[theme]^ (examples: darkly, slate, cosmo, spacelab, and superhero.
 // See https://bootswatch.com for current theme names.)
 //import "/static/vendor/bootstrap/css/bootstrap.min.css";
-import "/static/vendor/fontawesome-free/css/all.css";
-import "/static/css/covid19python.css";
-import "/static/vendor/jquery/jquery.js";
-import "/static/vendor/popper.js/popper.js";
-import "/static/vendor/bootstrap/js/bootstrap.bundle.min.js";
-import "/static/vendor/fontawesome-free/js/all.js";
-import "/static/js/covid19python.js";
+import "/vendor/fontawesome-free/css/all.css";
+import "/css/covid19python.css";
+import "/vendor/jquery/jquery.js";
+import "/vendor/popper.js/popper.js";
+import "/vendor/bootstrap/js/bootstrap.bundle.min.js";
+import "/vendor/fontawesome-free/js/all.js";
+import "/js/covid19python.js";
