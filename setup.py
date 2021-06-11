22606b3fd01a02b830d914fba57ffb5b02cd62ab
thomaswoehlke <thomas.woehlke@gmail.com>
2021-06-11 20:16:25 +0200

    work

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import subprocess
 
 from setuptools import find_packages, setup
 
-version = '0.0.45'
+version = '0.0.46'
 
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
