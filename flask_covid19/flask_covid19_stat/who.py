176251f99900dcabcbdfce29a8e470272f5dfc71
thomaswoehlke <thomas.woehlke@gmail.com>
2021-06-12 21:52:52 +0200

    work

--- flask_covid19/flask_covid19_stat/who.py
+++ flask_covid19/flask_covid19_stat/who.py
@@ -1,10 +1,20 @@
 import os
+import logging
+import pandas as pd
 os.getcwd()
-# os.chdir()
 
 #os.system("pip install pandas")
 #os.system("pip install scipy")
 #os.system("pip install matplotlib")
 
-import pandas as pd
+### Daten einlesen #############################################################################
+# csv-Datei direkt in einen DataFrame einlesen
+who = pd.read_csv('../data/who/WHO.csv', sep=',')
+who.info()         # grundlegende Informationen über den Datensatz
+b = list(who.keys())
+print(b)
+print(who.value_counts())
+print(who.index)
+print(who.head())
+print(who)
 
