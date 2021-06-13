b10f2aaa6a394e280cfba06000b69b1183368bef
thomaswoehlke <thomas.woehlke@gmail.com>
2021-06-13 11:46:48 +0200

    work

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import subprocess
 
 from setuptools import find_packages, setup
 
-version = '0.0.49'
+version = '0.0.50'
 
 scripts_dir = 'flask_covid19'+os.sep+'flask_covid19_build'+os.sep+'scripts'+os.sep
 pip_requirements_dir = 'flask_covid19'+os.sep+'flask_covid19_build'+os.sep+'requirements'
