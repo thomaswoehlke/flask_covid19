97f69382b794aae87ce02f529f6354bb08adf735
thomaswoehlke <thomas.woehlke@gmail.com>
2021-07-08 22:43:48 +0200

    work

--- flask_covid19_client/who.R
+++ flask_covid19_client/who.R
@@ -1,14 +1,14 @@
-install.packages("DBI")
-install.packages('RODBCDBI')
-install.packages("dbplyr")
-install.packages('tibble')
-install.packages('shiny')
-install.packages('pool')
-install.packages('RMySQL')
-install.packages('RPostgreSQL')
-install.packages('RSQLite')
-install.packages('RMySQL')
-install.packages("RMariaDB")
+# install.packages("DBI")
+# install.packages('RODBCDBI')
+# install.packages("dbplyr")
+# install.packages('tibble')
+# install.packages('shiny')
+# install.packages('pool')
+# install.packages('RMySQL')
+# install.packages('RPostgreSQL')
+# install.packages('RSQLite')
+# install.packages('RMySQL')
+# install.packages("RMariaDB")
 
 
 library(DBI)
@@ -20,9 +20,11 @@ library(dplyr)
 library(pool)
 
 library(odbc)
-sort(unique(odbcListDrivers()[[1]]))
-con <- dbConnect(odbc::odbc(),
-                 .connection_string = "Driver={MariaDB Unicode};DB={flask_covid19_rki},Server={tw-asus7},UID={flask_covid19},PWD={flask_covid19pwd},Port=3306",
-                 timeout = 10)
-dbGetQuery(con,"select * from rki_import_flat where landkreis = 'SK Bochum' order by meldedatum__datum desc")
+library(RMariaDB)
+
+# sort(unique(odbcListDrivers()[[1]]))
+
+con <- dbConnect(odbc::odbc(), .connection_string = "Driver={MariaDB Unicode};DB={flask_covid19_rki},Server={localhost},UID={flask_covid19},PWD={flask_covid19pwd},Port=3306",timeout=10)
+
+q = dbGetQuery(con,"select * from rki_import_flat where landkreis = 'SK Bochum' order by meldedatum__datum desc")
 
