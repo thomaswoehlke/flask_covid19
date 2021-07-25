d556c03a3b3987386be08f88075c82ace762add4
thomaswoehlke <thomas.woehlke@gmail.com>
2021-07-25 12:29:17 +0200

    work

---
+++ flask_covid19/app_web/static/index.js
@@ -0,0 +1,11 @@
+import "/app_web/staticb/static/vendor/bootswatch/superhero/bootstrap.min.css";
+// Note: Replace ^[theme]^ (examples: darkly, slate, cosmo, spacelab, and superhero.
+// See https://bootswatch.com for current theme names.)
+//import "/static/vendor/bootstrap/css/bootstrap.min.css";
+import "/app_web/staticb/static/vendor/fontawesome-free/css/all.css";
+import "/app_web/staticb/static/css/flask_covid19.css";
+import "/app_web/staticb/static/vendor/jquery/jquery.js";
+import "/app_web/staticb/static/vendor/popper.js/popper.js";
+import "/app_web/staticb/static/vendor/bootstrap/js/bootstrap.bundle.min.js";
+import "/app_web/staticb/static/vendor/fontawesome-free/js/all.js";
+import "/app_web/staticb/static/js/flask_covid19.js";
