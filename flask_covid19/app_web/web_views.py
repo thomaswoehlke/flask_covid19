68ec3bb59404fb3202055c138844b2d3ca2cf045
thomaswoehlke <thomas.woehlke@gmail.com>
2021-08-13 20:01:18 +0200

    setup

--- flask_covid19/app_web/web_views.py
+++ flask_covid19/app_web/web_views.py
@@ -39,7 +39,7 @@ class BlueprintApplicationUrls:
 
     def __init__(self):
         app.logger.debug("------------------------------------------------------------")
-        app.logger.info(" ready: [WEB] BlueprintApplicationUrls ")
+        app.logger.info(" ready: [WEB] BloodmoneyApplicationUrls ")
         app.logger.debug("------------------------------------------------------------")
 
     @staticmethod
