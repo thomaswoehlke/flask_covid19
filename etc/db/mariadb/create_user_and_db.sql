a6e4d30a8ce9f6875324df88980eb463237329e3
thomaswoehlke <thomas.woehlke@gmail.com>
2021-08-13 08:54:54 +0200

    research linux

--- etc/db/mariadb/create_user_and_db.sql
+++ etc/db/mariadb/create_user_and_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ed9eaf4dd0a2a7668e64a849d08f780ad4becab5a250e4f2ea1e36bfa05ccd8
-size 5250
+oid sha256:3d6f3c3630c5fc2de5febe8396c742e964c4bef5705f71f8d2f8bec73b1e3219
+size 4870
---
+++ etc/db/mariadb/create_user_and_db_tw.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cfb722cc43b35a4912c91d5e0f2a0a4714aa5d9a98d91113b15b64992ca4af58
+size 397
