0fb8c86086d16ef4dc92083202fd8cd28d8f2b31
thomaswoehlke <thomas.woehlke@gmail.com>
2021-12-09 21:26:03 +0100

    work

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
+import packaging.version
+from pallets_sphinx_themes import get_version
+from pallets_sphinx_themes import ProjectLink
 
 # -- Project information -----------------------------------------------------
 
@@ -23,13 +26,15 @@ author = 'Thomas Woehlke'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.72'
-
+version = "0.0.72"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc = "index"
+
 extensions = [
     "myst_parser",
     "pallets_sphinx_themes",
@@ -40,6 +45,48 @@ source_suffix = {
     ".txt": "markdown",
     ".md": "markdown",
 }
+
+autodoc_typehints = "description"
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "werkzeug": ("https://werkzeug.palletsprojects.com/", None),
+    "click": ("https://click.palletsprojects.com/", None),
+    "jinja": ("https://jinja.palletsprojects.com/", None),
+    "itsdangerous": ("https://itsdangerous.palletsprojects.com/", None),
+    "sqlalchemy": ("https://docs.sqlalchemy.org/", None),
+    "wtforms": ("https://wtforms.readthedocs.io/", None),
+    "blinker": ("https://pythonhosted.org/blinker/", None),
+}
+
+issues_github_path = "pallets/flask"
+
+# HTML -----------------------------------------------------------------
+
+html_theme = "flask"
+html_theme_options = {"index_sidebar_logo": False}
+html_context = {
+    "project_links": [
+        ProjectLink("Donate", "https://palletsprojects.com/donate"),
+        ProjectLink("PyPI Releases", "https://pypi.org/project/Flask/"),
+        ProjectLink("Source Code", "https://github.com/pallets/flask/"),
+        ProjectLink("Issue Tracker", "https://github.com/pallets/flask/issues/"),
+        ProjectLink("Website", "https://palletsprojects.com/p/flask/"),
+        ProjectLink("Twitter", "https://twitter.com/PalletsTeam"),
+        ProjectLink("Chat", "https://discord.gg/pallets"),
+    ]
+}
+html_sidebars = {
+    "index": ["project.html", "localtoc.html", "searchbox.html", "ethicalads.html"],
+    "**": ["localtoc.html", "relations.html", "searchbox.html", "ethicalads.html"],
+}
+singlehtml_sidebars = {"index": ["project.html", "localtoc.html", "ethicalads.html"]}
+html_static_path = ["_static"]
+html_favicon = "_static/flask-icon.png"
+html_logo = "_static/flask-icon.png"
+html_title = f"Flask Documentation ({version})"
+html_show_sourcelink = False
+
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -61,3 +108,43 @@ html_theme = 'flask'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# LaTeX ----------------------------------------------------------------
+
+latex_documents = [(master_doc, f"Flask-{version}.tex", html_title, author, "manual")]
+
+# Local Extensions -----------------------------------------------------
+
+
+def github_link(name, rawtext, text, lineno, inliner, options=None, content=None):
+    app = inliner.document.settings.env.app
+    release = app.config.release
+    base_url = "https://github.com/pallets/flask/tree/"
+
+    if text.endswith(">"):
+        words, text = text[:-1].rsplit("<", 1)
+        words = words.strip()
+    else:
+        words = None
+
+    if packaging.version.parse(release).is_devrelease:
+        url = f"{base_url}main/{text}"
+    else:
+        url = f"{base_url}{release}/{text}"
+
+    if words is None:
+        words = url
+
+    from docutils.nodes import reference
+    from docutils.parsers.rst.roles import set_classes
+
+    options = options or {}
+    set_classes(options)
+    node = reference(rawtext, words, refuri=url, **options)
+    return [node], []
+
+
+def setup(app):
+    app.add_role("gh", github_link)
+
