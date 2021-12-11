1457f7f663bf385ac75771cf2baba7bfe6869eb1
thomaswoehlke <thomas.woehlke@gmail.com>
2021-12-11 19:30:13 +0100

    work

--- docs/conf.py
+++ docs/conf.py
@@ -24,8 +24,8 @@ copyright = "2021, Thomas Woehlke"
 author = "Thomas Woehlke"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.74"
-version = "0.0.74"
+release = "0.0.75"
+version = "0.0.75"
 
 # -- General configuration ---------------------------------------------------
 
