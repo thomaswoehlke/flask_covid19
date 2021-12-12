bd571046f7b7f61b16c76505e75853345ae7d39a
thomaswoehlke <thomas.woehlke@gmail.com>
2021-12-12 20:53:58 +0100

    refactoring: rename directory flask_covid19 to project

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ def create_app():
     return app
 
 
-@app.cli.command("app-create-user")
+@app.cli.command("create-user")
 def create_user():
+    """[APP] create user"""
     with app.app_context():
         db.create_all()
         # cache.clear()
@@ -32,92 +33,163 @@ def create_user():
 
 @app.cli.command("all-download")
 def all_download():
+    """[ALL] download"""
     with app.app_context():
         all_dispachter_matrix_service.download()
 
 
 @app.cli.command("all-import")
-def all_download():
+def all_import():
+    """[ALL] import file"""
     with app.app_context():
         all_dispachter_matrix_service.import_file()
 
 
 @app.cli.command("all-update")
-def all_full_update():
+def all_update():
+    """[ALL] update"""
     with app.app_context():
         all_dispachter_matrix_service.update()
 
 
 @app.cli.command("all-update-full")
 def all_full_update():
+    """[ALL] full update"""
     with app.app_context():
         all_dispachter_matrix_service.full_update()
 
 
-@app.cli.command("all-delete-last-day")
-def all_delete_last_day():
-    with app.app_context():
-        all_dispachter_matrix_service.delete_last_day()
-
-
 @app.cli.command("who-download")
 def who_download():
+    """[WHO] download"""
     with app.app_context():
         who_service.download()
 
 
 @app.cli.command("who-import")
 def who_import_file():
+    """[WHO] import file"""
     with app.app_context():
         who_service.import_file()
 
 
-@app.cli.command("who-update")
+@app.cli.command("who-update-full")
 def who_full_update():
+    """[WHO] full update"""
     with app.app_context():
         who_service.full_update()
 
 
-@app.cli.command("who-update-full")
+@app.cli.command("who-update")
 def who_update():
+    """[WHO] update"""
     with app.app_context():
         who_service.update()
 
 
-@app.cli.command("who-delete-last-day")
-def who_delete_last_day():
+@app.cli.command("ecdc-download")
+def ecdc_download():
+    """[ECDC] download"""
     with app.app_context():
-        who_service.delete_last_day()
+        ecdc_service.download()
+
+
+@app.cli.command("ecdc-import")
+def ecdc_import_file():
+    """[ECDC] import file"""
+    with app.app_context():
+        ecdc_service.import_file()
+
+
+@app.cli.command("ecdc-update-full")
+def ecdc_full_update():
+    """[ECDC] full update"""
+    with app.app_context():
+        ecdc_service.full_update()
+
+
+@app.cli.command("ecdc-update")
+def ecdc_update():
+    """[ECDC] update"""
+    with app.app_context():
+        ecdc_service.update()
 
 
 @app.cli.command("owid-download")
 def owid_download():
+    """[OWID] download"""
     with app.app_context():
         owid_service.download()
 
 
-@app.cli.command("owid-import")
-def owid_import_file():
-    with app.app_context():
-        owid_service.import_file()
-
-
 @app.cli.command("owid-update-full")
 def owid_full_update():
+    """[OWID] full update"""
     with app.app_context():
         owid_service.full_update()
 
 
 @app.cli.command("owid-update")
 def owid_update():
+    """[OWID] update"""
     with app.app_context():
         owid_service.update()
 
 
-@app.cli.command("owid-delete-last-day")
-def owid_delete_last_day():
+@app.cli.command("rki-download")
+def rki_download():
+    """[RKI] download"""
     with app.app_context():
-        owid_service.delete_last_day()
+        rki_service.download()
+
+
+@app.cli.command("rki-import")
+def rki_import_file():
+    """[RKI] import file"""
+    with app.app_context():
+        rki_service.import_file()
+
+
+@app.cli.command("rki-update-full")
+def rki_full_update():
+    """[RKI] full update"""
+    with app.app_context():
+        rki_service.full_update()
+
+
+@app.cli.command("rki-update")
+def rki_update():
+    """[RKI] update"""
+    with app.app_context():
+        rki_service.update()
+
+
+@app.cli.command("vaccination-download")
+def vaccination_download():
+    """[vaccination] download"""
+    with app.app_context():
+        vaccination_service.download()
+
+
+@app.cli.command("vaccination-import")
+def vaccination_import_file():
+    """[vaccination] import file"""
+    with app.app_context():
+        vaccination_service.import_file()
+
+
+@app.cli.command("vaccination-update-full")
+def vaccination_full_update():
+    """[vaccination] full update"""
+    with app.app_context():
+        vaccination_service.full_update()
+
+
+@app.cli.command("vaccination-update")
+def vaccination_update():
+    """[vaccination] update"""
+    with app.app_context():
+        vaccination_service.update()
 
 
 # ---------------------------------------------------------------------------------
