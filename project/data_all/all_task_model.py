f8c6f3f574f8a2f0bb61c184b471b71aa6093312
thomaswoehlke <thomas.woehlke@gmail.com>
2021-12-20 19:29:43 +0100

    work

--- project/data_all/all_task_model.py
+++ project/data_all/all_task_model.py
@@ -143,6 +143,6 @@ class Task(db.Model, AllEntityMixinBase):
     @classmethod
     def notifications_get(cls, page: int):
         return db.session.query(cls)\
-            .filter(and_(cls.datum_finished.is_not(None), cls.new_notification))\
+            .filter(cls.new_notification)\
             .order_by(cls.id).paginate(page, per_page=items_per_page)
 
