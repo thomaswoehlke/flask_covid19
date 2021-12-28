b89c2140559af44468a8bd45eb8012c7e95851f3
thomasoehlke <thomas.woehlke@rub.de>
2021-12-28 11:33:07 +0100

    research windows

--- mq.cmd
+++ mq.cmd
@@ -1,8 +1,7 @@
-rem .\venv\Scripts\activate
-
-start /max cmd /t:8f /k "celery --app app.celery worker --pool eventlet --loglevel INFO"
 
 set FLASK_APP=app.py
 set FLASK_ENV=development
+
+start /max cmd /t:8f /k "celery --app app.celery worker --pool eventlet --loglevel INFO"
 
 start /max cmd /t:90 /k "flask run"
