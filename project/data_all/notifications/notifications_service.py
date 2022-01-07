454d17dc157f355d755a8033d8fd70dc83da4016
thomaswoehlke <thomas.woehlke@gmail.com>
2022-01-07 19:26:11 +0100

    work

--- project/data_all/notifications/notifications_service.py
+++ project/data_all/notifications/notifications_service.py
@@ -1,5 +1,5 @@
 from project.app_bootstrap.database import app
-from project.data_all.task.all_task_model import Task
+from project.data_all.notifications.all_task_model import Task
 
 
 class TaskService:
