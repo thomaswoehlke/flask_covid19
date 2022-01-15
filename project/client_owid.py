ba12b0b9ba2ed5017825881507dc5ebfae5e1f28
thomaswoehlke <thomas.woehlke@gmail.com>
2022-01-15 16:49:10 +0100

    work

--- project/client_owid.py
+++
@@ -1,9 +0,0 @@
-#!/web_user/bin/env python
-from project.web.cli.data_runner import Runner
-from project.data_all.services.all_config import BlueprintConfig
-
-
-cfg = BlueprintConfig.create_config_for_owid()
-oo = Runner(cfg)
-oo.prepare()
-oo.daten_einlesen()
