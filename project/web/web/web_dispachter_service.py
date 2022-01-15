75df8b127403eeb9fc9e3c3018e4b9348961b4e3
thomaswoehlke <thomas.woehlke@gmail.com>
2022-01-15 21:29:04 +0100

    work

--- project/web/web/web_dispachter_service.py
+++ project/web/web/web_dispachter_service.py
@@ -2,7 +2,7 @@ from project.data.database import app, db
 from project.web_admin.app_admin_service import AdminService
 from project.web_user.user_service import UserService
 from project.web.web.web_service import WebService
-from project.data_all.services.all_service import AllServiceDispachterMatrix
+from project.data_all.services.all_service_dispachter_matrix import AllServiceDispachterMatrix
 from project.data_ecdc.services.ecdc_service import EcdcService
 from project.data_owid.services.owid_service import OwidService
 from project.data_rki.services.rki_service import RkiService
