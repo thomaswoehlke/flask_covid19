b9c05110cb8c14b9467b6c885e02a0332c49239a
thomaswoehlke <thomas.woehlke@gmail.com>
2022-01-15 21:54:07 +0100

    work

---
+++ project/data_ecdc/factories/ecdc_model_location_group.py
@@ -0,0 +1,12 @@
+from project.data_ecdc.model import EcdcContinent
+
+
+class EcdcContinentFactory:
+    @classmethod
+    def create_new(cls, location_group_str: str):
+        o = EcdcContinent(
+            location_group=location_group_str,
+            processed_update=False,
+            processed_full_update=False,
+        )
+        return o
--- project/data_ecdc/model/__init__.py
+++ project/data_ecdc/model/__init__.py
@@ -5,17 +5,6 @@ from project.data_ecdc.model.ecdc_model_location import EcdcCountry
 from project.data_ecdc.model.ecdc_model_location_group import EcdcContinent
 
 
-class EcdcContinentFactory:
-    @classmethod
-    def create_new(cls, location_group_str: str):
-        o = EcdcContinent(
-            location_group=location_group_str,
-            processed_update=False,
-            processed_full_update=False,
-        )
-        return o
-
-
 class EcdcCountryFactory:
     @classmethod
     def create_new(cls, c: [], my_continent: EcdcContinent):
