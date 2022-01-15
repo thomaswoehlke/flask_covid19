ea78361296c440defe891176f83929505e888dfb
thomaswoehlke <thomas.woehlke@gmail.com>
2022-01-15 15:47:08 +0100

    work

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,31 +15,43 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 files: {
-                    'node_modules/jquery-fancybox/source/css/jquery.fancybox.css': 'node_modules/jquery-fancybox/source/scss/jquery.fancybox.scss'
+                    'node_modules/jquery-fancybox/source/css/jquery.fancybox.css':
+                      'node_modules/jquery-fancybox/source/scss/jquery.fancybox.scss'
                 }
             }
         },
         copy: {
             main: {
                 files: [
-                    {expand: true, cwd: 'node_modules/jquery/dist/', src: ['**'], dest: 'static/vendor/jquery'},
-                    {expand: true, cwd: 'node_modules/popper.js/dist/', src: ['**'], dest: 'static/vendor/popper.js'},
-                    {expand: true, cwd: 'node_modules/bootstrap/dist/', src: ['**'], dest: 'static/vendor/bootstrap'},
-                    {expand: true, cwd: 'node_modules/@fortawesome/fontawesome-free/', src: ['**'], dest: 'static/vendor/fontawesome-free'},
-                    {expand: true, cwd: 'node_modules/bootswatch/dist/', src: ['**'], dest: 'static/vendor/bootswatch'},
+                    {expand: true, cwd: 'node_modules/jquery/dist/',
+                      src: ['**'],
+                      dest: 'static/vendor/jquery'},
+                    {expand: true, cwd: 'node_modules/popper.js/dist/',
+                      src: ['**'],
+                      dest: 'static/vendor/popper.js'},
+                    {expand: true, cwd: 'node_modules/bootstrap/dist/',
+                      src: ['**'],
+                      dest: 'static/vendor/bootstrap'},
+                    {expand: true, cwd: 'node_modules/@fortawesome/fontawesome-free/',
+                      src: ['**'],
+                      dest: 'static/vendor/fontawesome-free'},
+                    {expand: true, cwd: 'node_modules/bootswatch/dist/',
+                      src: ['**'],
+                      dest: 'static/vendor/bootswatch'},
                 ],
             },
         },
     });
 
     // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // grunt.loadNpmTasks('grunt-contrib-less');
+    // grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-run-grunt');
-    grunt.loadNpmTasks('grunt-contrib-versioning2');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-versioning2');
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
