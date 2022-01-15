4d83c31cc15beaf3a6023110e7e5cffcf3bccc3c
thomaswoehlke <thomas.woehlke@gmail.com>
2022-01-15 12:16:48 +0100

    work

--- etc/db/postgresql/create_user_and_database__flask_covid19.sql
+++ etc/db/postgresql/create_user_and_database__flask_covid19.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d6f550e4014c51d6ab006b9b6f10cf89c679d1cc27bd0d4d1839d2ba75cdb08
-size 993
+oid sha256:4d03c64db44d5ad217201d555acc5ed8c539e1fee197197fcd2f0447accde7b2
+size 1246
