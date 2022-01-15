f39925b2d051dc5f86470a7d459ecc96d328e6b4
thomaswoehlke <thomas.woehlke@gmail.com>
2022-01-15 23:23:56 +0100

    work

--- project/web_admin/web_admin_views.py
+++ project/web_admin/web_admin_views.py
@@ -9,14 +9,23 @@ from flask_login import login_required
 
 from project.data.database import app, celery, db
 from project.web.model.web_model_transient import WebPageContent
+from project.web_admin.services.database_import_status import \
+    DatabaseImportStatusService
+from project.web_admin.services.database_table_row_count import \
+    DatabaseTableRowCountService
 from project.web_admin.web_admin_service import WebAdminService
 
+
+database_table_row_count = DatabaseTableRowCountService(db)
+database_import_status = DatabaseImportStatusService(db)
 admin_service = WebAdminService(db)
 
 drop_and_create_data_again = True
 
 app_web_admin = Blueprint(
-    "web_admin", __name__, template_folder="templates", url_prefix="/app/admin"
+    "web_admin", __name__,
+    template_folder="templates",
+    url_prefix="/app/admin"
 )
 
 
@@ -50,7 +59,7 @@ class AppAdminUrls:
     @login_required
     def url_admin_database_table_row_count():
         page_info = WebPageContent("Admin", "DB Row Count")
-        db_table_row_count = admin_service.database_table_row_count()
+        db_table_row_count = database_table_row_count.database_table_row_count()
         return render_template(
             "app_web_admin/table_row_count/status.html",
             db_table_row_count=db_table_row_count,
@@ -62,12 +71,42 @@ class AppAdminUrls:
     @login_required
     def url_admin_database_import_status():
         page_info = WebPageContent("Admin", "DB Import Status")
-        db_import_status = admin_service.database_import_status()
+        db_import_status = database_import_status.database_import_status()
         return render_template(
             "app_web_admin/database_import_status.html",
             db_import_status=db_import_status,
             page_info=page_info
         )
+
+    @staticmethod
+    @app_web_admin.route("/task/database/dump")
+    @login_required
+    def url_task_admin_database_dump():
+        app.logger.info("url_task_admin_database_dump [start]")
+        admin_service.database_dump()
+        flash("admin_service.run_admin_database_dump started")
+        app.logger.info("url_task_admin_database_dump [done]")
+        return redirect(url_for("web_admin.url_admin_tasks"))
+
+    @staticmethod
+    @app_web_admin.route("/task/database/reimport")
+    @login_required
+    def url_task_admin_database_dump_reimport():
+        app.logger.info("url_task_admin_database_dump_reimport [start]")
+        admin_service.database_dump_reimport()
+        flash("admin_service.run_admin_database_import started")
+        app.logger.info("url_task_admin_database_dump_reimport [done]")
+        return redirect(url_for("web_admin.url_admin_tasks"))
+
+    @staticmethod
+    @app_web_admin.route("/task/database/drop_create")
+    @login_required
+    def url_task_admin_database_dropcreate():
+        app.logger.info("url_task_admin_database_dropcreate [start]")
+        admin_service.database_drop_and_create()
+        flash("admin_service.run_admin_database_drop started")
+        app.logger.info("url_task_admin_database_dropcreate [done]")
+        return redirect(url_for("web_admin.url_admin_tasks"))
 
 
 app_admin_urls = AppAdminUrls()
@@ -112,36 +151,6 @@ class AppAdminTaskUrls:
         app_admin_tasks.task_admin_alive_message.apply_async()
         flash("alive_message_task started")
         app.logger.info("url_task_admin_message_start [done]")
-        return redirect(url_for("web_admin.url_admin_tasks"))
-
-    @staticmethod
-    @app_web_admin.route("/task/database/dump")
-    @login_required
-    def url_task_admin_database_dump():
-        app.logger.info("url_task_admin_database_dump [start]")
-        admin_service.database_dump()
-        flash("admin_service.run_admin_database_dump started")
-        app.logger.info("url_task_admin_database_dump [done]")
-        return redirect(url_for("web_admin.url_admin_tasks"))
-
-    @staticmethod
-    @app_web_admin.route("/task/database/reimport")
-    @login_required
-    def url_task_admin_database_dump_reimport():
-        app.logger.info("url_task_admin_database_dump_reimport [start]")
-        admin_service.database_dump_reimport()
-        flash("admin_service.run_admin_database_import started")
-        app.logger.info("url_task_admin_database_dump_reimport [done]")
-        return redirect(url_for("web_admin.url_admin_tasks"))
-
-    @staticmethod
-    @app_web_admin.route("/task/database/drop_create")
-    @login_required
-    def url_task_admin_database_dropcreate():
-        app.logger.info("url_task_admin_database_dropcreate [start]")
-        admin_service.database_drop_and_create()
-        flash("admin_service.run_admin_database_drop started")
-        app.logger.info("url_task_admin_database_dropcreate [done]")
         return redirect(url_for("web_admin.url_admin_tasks"))
 
 
